6bd118c09578570ce679440dd0c8b775e71cbea4
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-26 11:10:46 -0700

    Add validation logic to new/edit person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,12 @@
 class Person < ActiveRecord::Base
 
+  validates :name_title?, presence: true
+  validates :last_name, presence: true
+
+  def name_title?
+    first_name.present? || title.present?
+  end
+
   def full_name
     "#{title} #{first_name} #{last_name}"
   end
