e051cbbf1799d5160ccea13899c28ff9e0682dc5
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-27 11:42:05 -0700

    Role can be edited to the same role without error

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -7,8 +7,8 @@ class Assignment < ActiveRecord::Base
   validates :location_id, :person_id, :role, presence: true
 
   def unique_location
-    if person.assignments.find_by(location_id: location_id, role: role)
-      errors.add(:base, "People cannot be assigned to the same location with the same role")
+    if  person.assignments.where('id <> ?', id).find_by(location_id: location_id, role: role)
+        errors.add(:base, "People cannot be assigned to the same location with the same role")
     end
   end
 
